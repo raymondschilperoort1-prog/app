c616eb2abc13f2981d2ff898896e33696b5378f1
raymondschilperoort1-prog <raymond.schilperoort1@gmail.com>
2026-05-05 12:28:45 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+function App() {
+  return <div>WORKING</div>;
+}
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default App;
