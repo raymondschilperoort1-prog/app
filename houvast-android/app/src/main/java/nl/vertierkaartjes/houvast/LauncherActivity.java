a1581f8c05b9f3f8127195abe0faa3da44ed94c8
raymondschilperoort1-prog <raymond.schilperoort1@gmail.com>
2026-07-23 12:45:51 +0200

    Add Houvast Trusted Web Activity launcher

---
+++ houvast-android/app/src/main/java/nl/vertierkaartjes/houvast/LauncherActivity.java
@@ -0,0 +1,9 @@
+package nl.vertierkaartjes.houvast;
+
+/**
+ * Dunne native toegangspoort naar de Houvast PWA via een Trusted Web Activity.
+ * De webapp blijft de bron van de gebruikersinterface en functionaliteit.
+ */
+public final class LauncherActivity
+        extends com.google.androidbrowserhelper.trusted.LauncherActivity {
+}
